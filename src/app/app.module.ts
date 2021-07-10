67b8c04b694f7bf15b8f5bd3dff1209992a07adf
Ash <ashfaq.choudhry@gmail.com>
2021-07-10 13:13:09 -0500

    added components to module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './components/about.component';
+import { CalendarComponent } from './components/calendar.component';
+import { DashboardComponent } from './components/dashboard.component';
+import { EmailComponent } from './components/email.component';
+import { HomeComponent } from './components/home.component';
+import { RolesComponent } from './components/roles.component';
+import { SettingsComponent } from './components/settings.component';
+import { UsersComponent } from './components/users.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    AboutComponent,
+    CalendarComponent,
+    DashboardComponent,
+    EmailComponent,
+    HomeComponent,
+    RolesComponent,
+    SettingsComponent,
+    UsersComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
